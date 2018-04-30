ff4f5c9592db99e99167d614cbf38bb0dd1207a8
angelo.la-greca@sogeti.com <angelo.la-greca@sogeti.com>
2018-04-30 22:04:44 +0200

    added microservice admin

---
+++ npsg_backend/npsg-admin/src/main/java/com/npsg/admin/NpsgAdminApplication.java
@@ -0,0 +1,12 @@
+package com.npsg.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpsgAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpsgAdminApplication.class, args);
+	}
+}
